33d6ea3a1395d58c6587ff9ea9a28ed020537197
care4u <sarveshchavan777@gmail.com>
2017-10-18 06:55:06 -0500

    comments added

--- app/src/main/java/com/sarvesh/downloadimageapp/MyAsyncTask.java
+++ app/src/main/java/com/sarvesh/downloadimageapp/MyAsyncTask.java
@@ -16,43 +16,54 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class MyAsyncTask extends AsyncTask <String,Integer,Boolean>{
+public class MyAsyncTask extends AsyncTask<String, Integer, Boolean> {
     private int totalBytesRead = -1;
-    private  int lengthOfFile = 0;
-    int progressValue=0;
+    private int lengthOfFile = 0;
+    int progressValue = 0;
     Activity activity;
 
-    MyAsyncTask(Activity activity){
-        this.activity=activity;
+    MyAsyncTask(Activity activity) {
+        this.activity = activity;
     }
 
-    public void onAttach(Context context){
+    public void onAttach(Context context) {
         this.activity = (Activity) context;
     }
 
-    public void onDetach(){
-        activity=null;
+    public void onDetach() {
+        activity = null;
     }
 
     @Override
     protected Boolean doInBackground(String... objects) {
         URL url = null;
-        int read=-1;
+        int read = -1;
 
         try {
-            url=new URL(objects[0]);
+            url = new URL(objects[0]);
+            //Establish the connection
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            // getting file length
             lengthOfFile = httpURLConnection.getContentLength();
+
+            // input stream to read file
             InputStream inputStream = httpURLConnection.getInputStream();
-            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath()+"/"+ Uri.parse((String) objects[0]).getLastPathSegment());
+
+            // Output stream to write file
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/" + Uri.parse((String) objects[0]).getLastPathSegment());
             FileOutputStream fileOutputStream = new FileOutputStream(file);
 
-            byte [] mybytes=new byte[1024];
+            //byte array to store number of byte read at each shot
+            byte[] mybytes = new byte[1024];
 
-            while((read = inputStream.read(mybytes))!=-1){
-                totalBytesRead+=read;
+            //Each iteration it will fill the array with the number of bytes read and will return the int to read variable
+            //when it will encounter the end of the file it will return -1 and we will terminate the loop
+            //the read() method will store number of byte read each round in the array mybytes
+            while ((read = inputStream.read(mybytes)) != -1) {
+                totalBytesRead += read;
                 publishProgress(totalBytesRead);
-                fileOutputStream.write(mybytes,0,read);
+                fileOutputStream.write(mybytes, 0, read);
             }
 
         } catch (IOException e) {
@@ -64,10 +75,10 @@ public class MyAsyncTask extends AsyncTask <String,Integer,Boolean>{
     @Override
     protected void onProgressUpdate(Integer... values) {
 
-        if(activity!=null){
-            progressValue = (int) (((double)values[0]/lengthOfFile)*100);
+        if (activity != null) {
+            progressValue = (int) (((double) values[0] / lengthOfFile) * 100);
             Log.d("Progressfuck", String.valueOf((double) totalBytesRead));
-            ((MainActivity)activity).updateProgress(progressValue);
+            ((MainActivity) activity).updateProgress(progressValue);
         }
     }
 }
